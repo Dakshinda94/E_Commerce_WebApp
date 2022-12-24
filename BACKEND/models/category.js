e43b78ce2459decb9d039ff5d426386a82546b40
Dakshinda94 <98576741+Dakshinda94@users.noreply.github.com>
2022-12-24 21:59:44 +0530

    Update category.js

--- BACKEND/models/category.js
+++ BACKEND/models/category.js
@@ -3,12 +3,16 @@ const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
     name:{
         type: String,
+
         required: true,
+
         trim: true
     },
     slug:{
         type: String,
+
         required: true,
+        
         unique: true
     },
     parentId:{
